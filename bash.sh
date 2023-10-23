7a2b2d70b91e6afc56a8b66063c9acfaa59041ad
Yousof.A.Asadi <yosofasady2@gmail.com>
2023-10-23 14:54:50 +0330

    v0.2.2

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ ! -d ${icons} ]]; then
+mkdir icons
+fi
+data="/home/flatlife/PycharmProjects/MaktabsharifMiniProject3/icons.txt"
+file=$data
+while read -r line; do
+echo -e "$line\n"
+mv $line "/home/flatlife/PycharmProjects/MaktabsharifMiniProject3/icons"
+done <$file
