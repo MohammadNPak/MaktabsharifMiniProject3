77e3d633a5017c760b47697f9c762315afabb931
ImanSMD <110537853+ImanSMD@users.noreply.github.com>
2023-10-28 23:54:05 +0330

    wirte on icon.txt

--- back/main.py
+++ back/main.py
@@ -1,17 +1,28 @@
 #  part 1 done 
-# sort files by (A-Z)
+# sort files by (A-Z) : done
+#find *-icon.* , *-icon-l.*
 from pathlib import Path
 
 
 
-from glob import glob
   
-address = '/Users/SMD/Desktop/GW2/MaktabsharifMiniProject3/front/images/*'
 
 def searchSorting (address): 
-    yield (name for name in sorted(glob(address)))     
+    return  sorted(Path(address).glob("*"))
 
 
-sorted_list = searchSorting(address=address)
+addrs = '/Users/SMD/Desktop/GW2/MaktabsharifMiniProject3/front/images/'
+# pathInput = input("Please Enter your Path: ")
+# address = Path.glob(addrs,recursive=True ,include_hidden=True )
+sorted_list = searchSorting(addrs)
+# for i in sorted_list:
+#     print(i)
+# print(sorted_list)
 
-print(sorted_list)
+icon = Path(addrs).glob("*icon*")
+with open("icon.txt" , 'w') as file:
+
+    for i in icon :
+        file.write(str(i)+"\n")
+
+
