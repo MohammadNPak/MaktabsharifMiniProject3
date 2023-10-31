db41dfb2fe4cabbf1f7aa6cbbf647543a863964b
ehsan <ehsan.1383asg63@yahoo.com>
2023-10-31 14:37:03 +0330

    Optimizing and updating the file commit checker

--- commit_checker.py
+++ commit_checker.py
@@ -1,7 +1,29 @@
 import requests
 import json
-token = "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"
-headers = {'Authorization':f'Bearer {token}','accept': 'application/vnd.github+json'}
-url = "https://api.github.com/repos/MohRezam/MaktabsharifMiniProject3/commits/ehsan-asgari"
-login = requests.get(url,headers=headers)
-print(login.text)
+from typing import Optional
+class ApiClient:
+    url = "https://api.github.com/repos/MohRezam/MaktabsharifMiniProject3/commits"
+    def __init__(self,api_token:str) -> None:
+        self.api_token = api_token
+        self.session = requests.session()
+    def connect(self) -> None:
+        self.session.headers.update({'Authorization':f'Bearer {self.api_token}','accept': 'application/vnd.github+json'})
+    def disconnect(self) -> None:
+        self.session.close()
+    def get_commit_data(self) -> dict:
+        commit_data = requests.get(self.url)
+        commit_data.raise_for_status()
+        return commit_data.json()
+class ApiClientConnection:
+    def __init__(self,api_token:str) -> None:
+        self.api_token = api_token
+        self.api_client: Optional[ApiClient]=None
+    def __enter__(self) -> ApiClient:
+        self.api_client = ApiClient(self.api_token)
+        self.api_client.connect()
+        return self.api_client
+    def __exit__(self, exc_type, exc_value, exc_tb) -> None:
+        self.api_client.disconnect()
+
+with ApiClientConnection("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") as api:
+    print(api.get_commit_data())
