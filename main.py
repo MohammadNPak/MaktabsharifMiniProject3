a115bcc1bbf4ee59db758d01521b732112ef3f5c
Miralireza <seyyedi.alireza77@gmail.com>
2023-11-01 15:02:58 +0330

    final version (added q3)

--- main.py
+++ main.py
@@ -4,29 +4,17 @@ import os
 import glob
 #Q2-1
 CODE_DIR= os.path.dirname(os.path.abspath(__file__))
-# DIR_IMAGE = os.path.join(CODE_DIR,"front/images/")
-# list_of_files = sorted(  glob.glob(DIR_IMAGE+'*') )
-
+DIR_IMAGE = os.path.join(CODE_DIR,"front/images/")
+list_of_files = sorted(  glob.glob(DIR_IMAGE+'*') )
+for path in list_of_files :
+    print(path)
 #Q2-2
-# for path in list_of_files :
-#     print(path)
-# list_of_icons = glob.glob(DIR_IMAGE+'*icon*')
-# with open('icons.txt','w') as i:
-#     for image in list_of_icons:
-#         i.write(image+'\n')
 
-# print(DIR_IMAGE,CODE_DIR_IMAGE)
-# path ='/home/flatlife/PycharmProjects/MaktabsharifMiniProject3/front/images'
-# images = os.listdir(DIR_IMAGE)
+list_of_icons = glob.glob(DIR_IMAGE+'*icon*')
+with open('icons.txt','w') as i:
+    for image in list_of_icons:
+        i.write(image+'\n')
 
-
-#
-# # re.match(r"icon")
-# with open("icons.txt", "a") as file:
-#     for image in images:
-#         # print(str(image))
-#         if re.match(r".*icon.*", str(image)):
-#             file.writelines(DIR_IMAGE+str(image)+"\n")
 #Q2-4
 def get_html(CODE_DIR):
     DIR_FRONT = os.path.join(CODE_DIR,"front/")
