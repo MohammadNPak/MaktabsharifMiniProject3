e0ac35d5af8fda8921dde0ff81649325bc2ac543
ehsan-asg <127941116+ehsan-asg@users.noreply.github.com>
2023-11-01 18:15:36 +0330

    Update commit_checker.py

--- commit_checker.py
+++ commit_checker.py
@@ -2,7 +2,7 @@ import requests
 import json
 from typing import Optional
 class ApiClient:
-    url = "https://api.github.com/repos/MohRezam/MaktabsharifMiniProject3/branches"
+    url = "https://api.github.com/repos/MohRezam/MaktabsharifMiniProject3/commits"
     def __init__(self,api_token:str) -> None:
         self.api_token = api_token
         self.session = requests.session()
@@ -13,7 +13,8 @@ class ApiClient:
     def get_commit_data(self) -> dict:
         commit_data = requests.get(self.url)
         commit_data.raise_for_status()
-        return commit_data.json()[0]["commit"]
+        return commit_data.json()
+    
 class ApiClientConnection:
     def __init__(self,api_token:str) -> None:
         self.api_token = api_token
@@ -26,4 +27,21 @@ class ApiClientConnection:
         self.api_client.disconnect()
 
 with ApiClientConnection("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") as api:
-    print(api.get_commit_data())
+    commit_data_by_author = {}
+    for item in api.get_commit_data():
+            author = item['commit']['author']['name']
+            message = item['commit']['message']
+            author_email = item['commit']['author']['email']
+            timestamp = item['commit']['author']['date']
+            if author not in commit_data_by_author:
+               commit_data_by_author[author] = []
+
+            commit_data_by_author[author].append({
+                'email':author_email,
+                'message': message,
+                'timestamp': timestamp
+            })
+with open("commit_json.json", 'w') as json_file:
+    json.dump(commit_data_by_author, json_file, indent=4)
+
+print(f"commit_json file was successfully saved")
