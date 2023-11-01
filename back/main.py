cff6aea8c650cd616454de94a957e17a3b5c9da4
Amirznd <zenderoohamirz@gmail.com>
2023-11-01 14:49:18 +0330

    about added

--- back/main.py
+++ back/main.py
@@ -1,18 +1,16 @@
-#  part 1 done 
+#  part 1 done
 # sort files by (A-Z) : done
-#find *-icon.* , *-icon-l.*
+# find *-icon.* , *-icon-l.*
 from pathlib import Path
 
 
+def searchSorting(address):
+    return sorted(addrs.glob("*"))
 
-  
-
-def searchSorting (address): 
-    return  sorted(addrs.glob("*"))
 
 # Path.exists():
 #     pass
-addrs = Path('/Users/SMD/Desktop/GW2/MaktabsharifMiniProject3/front/images/')
+addrs = Path('../front/images/')
 # pathInput = input("Please Enter your Path: ")
 # address = Path.glob(addrs,recursive=True ,include_hidden=True )
 sorted_list = searchSorting(addrs)
@@ -21,18 +19,18 @@ sorted_list = searchSorting(addrs)
 # print(sorted_list)
 
 icon = addrs.glob("*icon*")
-with open("icon.txt" , 'w') as file:
+with open("icon.txt", 'w') as file:
 
-    for i in icon :
+    for i in icon:
         file.write(str(i)+"\n")
 
 
 newfile = Path('.') / "icon.txt"
-with newfile.open("w") as f: f.write()
+with newfile.open("w") as f:
+    f.write()
 
 a = newfile.touch()
 a.write_text()
-
 
 
 path = Path.cwd() / "shopping_list.md"
@@ -41,10 +39,8 @@ with path.open(mode="r", encoding="utf-8") as md_file:
     groceries = [line for line in content.splitlines() if line.startswith("*")]
 print("\n".join(groceries))
 
-write_text() opens the path and writes string data to it
+# write_text() opens the path and writes string data to it
 
-
-from pathlib import Path
 
 source = Path("hello.py")
 destination = Path("goodbye.py")
